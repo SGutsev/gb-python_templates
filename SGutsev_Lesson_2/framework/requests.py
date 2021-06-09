7c53373996240b04c2a7e25e1b7efdc23956112a
SGutsev <seregaguts@yandex.ru>
2021-06-09 21:26:15 +0300

    SGutsev_lesson_2 send

---
+++ SGutsev_Lesson_2/framework/requests.py
@@ -0,0 +1,64 @@
+# get requests
+class GetRequests:
+
+    @staticmethod
+    def parse_input(data: str):
+        result = {}
+        if data:
+            # делим параметры через &
+            params = data.split('&')
+            for item in params:
+                # делим ключ и значение через =
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    @staticmethod
+    def get_req_params(environ):
+        # получаем параметры запроса
+        query_string = environ['QUERY_STRING']
+        # превращаем параметры в словарь
+        request_params = GetRequests.parse_input(query_string)
+        return request_params
+
+
+# post requests
+class PostRequests:
+
+    @staticmethod
+    def parse_input(data: str):
+        result = {}
+        if data:
+            # делим параметры через &
+            params = data.split('&')
+            for item in params:
+                # делим ключ и значение через =
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    @staticmethod
+    def get_wsgi_input(env) -> bytes:
+        # получаем длину тела
+        content_length_data = env.get('CONTENT_LENGTH')
+        # приводим к int
+        content_length = int(content_length_data) if content_length_data else 0
+        # считываем данные, если они есть
+        data = env['wsgi.input'].read(content_length) if content_length > 0 else b''
+        return data
+
+    def parse_wsgi_input(self, data: bytes) -> dict:
+        result = {}
+        if data:
+            # декодируем данные
+            data_str = data.decode(encoding='utf-8')
+            # собираем их в словарь
+            result = self.parse_input(data_str)
+        return result
+
+    def get_req_params(self, environ):
+        # получаем данные
+        data = self.get_wsgi_input(environ)
+        # превращаем данные в словарь
+        data = self.parse_wsgi_input(data)
+        return data
