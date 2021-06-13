3335e27d2bc20dfcd7977d382595d68e353abbfb
SGutsev <seregaguts@yandex.ru>
2021-06-13 09:04:29 +0300

    SGutsev_lesson_4 send task

---
+++ SGutsev_Lesson_4/urls.py
@@ -0,0 +1,27 @@
+from datetime import date
+from views import Index, About, StudyPrograms, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList, CopyCourse
+
+
+# front controller
+def secret_front(request):
+    request['date'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+# Соответствие url обработчикам page controller, добавили новые
+routes = {
+    '/': Index(),
+    '/about/': About(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
+}
